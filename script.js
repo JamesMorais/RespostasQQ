3121200891900df2fae0766f171b347462d04e56
JamesMorais <capic.2022118tads0048@aluno.ifpi.edu.br>
2024-09-12 01:13:25 -0300

    correções nas funções

--- script.js
+++ script.js
@@ -63,11 +63,11 @@ function calcularQtdLitros() {
 }
 function calcularValorkg() {
     const peso = parseFloat(document.getElementById('peso').value);
-    resultado = document.getElementById('resultado6');
+    const resultado = document.getElementById('resultado6');
 
     if (peso > 0) {
         let valor = peso * 12
-        resultado.innerHTML = `O valor a ser page é: ${valor.toFixed(2)} reais`
+        resultado.innerHTML = `O valor a ser pago é: ${valor.toFixed(2)} reais`
     } else {
         resultado.innerHTML = 'Digite um valor válido'
     }
@@ -75,7 +75,7 @@ function calcularValorkg() {
 function calcularDiasPassados() {
     const dia = parseInt(document.getElementById('dia').value);
     const mes = parseInt(document.getElementById('mes').value);
-    resultado = document.getElementById('resultado7');
+    const resultado = document.getElementById('resultado7');
 
     if (dia > 0 && mes > 0) {
         let diasPassados = (mes - 1) * 30 + dia;
@@ -89,7 +89,7 @@ function calcularMediaP() {
     const nota1 = parseFloat(document.getElementById('nota1').value);
     const nota2 = parseFloat(document.getElementById('nota2').value);
     const nota3 = parseFloat(document.getElementById('nota3').value);
-    resultado = document.getElementById('resultado8');
+    const resultado = document.getElementById('resultado8');
 
     let peso1 = 1;
     let peso2 = 2;
@@ -97,7 +97,7 @@ function calcularMediaP() {
 
     if (nota1 >= 0 && nota2 >= 0 && nota3 >= 0) {
         let media = (nota1 * peso1 + nota2 * peso2 + nota3 * peso3) / (peso1 + peso2 + peso3)
-        resultado.innerHTML = `Média ponderada das notas: ${media}`
+        resultado.innerHTML = `Média ponderada das notas: ${media.toFixed(2)}`
     } else {
         resultado.innerHTML = 'Digite um valor válido'
     }
@@ -106,9 +106,9 @@ function calcularValorArrecadado() {
     const p = parseInt(document.getElementById('p').value);
     const m = parseInt(document.getElementById('m').value);
     const g = parseInt(document.getElementById('g').value);
-    resultado = document.getElementById('resultado9')
+    const resultado = document.getElementById('resultado9')
 
-    if (p >= 0 && m >= 0 && g > 0) {
+    if (p >= 0 && m >= 0 && g >=0) {
         let total = (p * 10 + m * 12 + g * 15);
         resultado.innerHTML = `O valor arrecadado é de ${total} reais`
     }else{
@@ -119,8 +119,8 @@ function calcularDistanciaPC(){
     const x1 = parseFloat(document.getElementById('x1').value);
     const y1 = parseFloat(document.getElementById('y1').value);
     const x2 = parseFloat(document.getElementById('x2').value);
-    const y2 = parseFloat(document.getElementById('x1').value);
-    resultado = document.getElementById('resultado10')
+    const y2 = parseFloat(document.getElementById('y2').value);
+    const resultado = document.getElementById('resultado10')
 
     let distanciaX = x2-x1
     let distanciaY = y2 - y1;
